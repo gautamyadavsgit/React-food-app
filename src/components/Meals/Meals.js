d4b7e4ecbf0774f073014255e90907cd7bf3369f
gautamyadavsgit <gautaminit@gmail.com>
2022-09-29 09:46:47 -0700

    changes saved

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,13 +1,15 @@
-import { Fragment } from "react";
-import MealsSummary from "./MeaslSummary";
-import AvailableMeals from "./AvailaibleMeals";
-const Meals = () => {
-return (
-    <Fragment>
-        <MealsSummary/>
-        <AvailableMeals/>
-    </Fragment>
-);
+import { Fragment } from 'react';
 
-}
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
 export default Meals;
